c323f033b34b26cff31624ceb5b290eda668a991
Hritikksharma89 <118102992+Hritikksharma89@users.noreply.github.com>
2024-03-26 10:23:08 +0530

    V1

--- src/lib/response.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express'
-import logger from './log'
-
-export const Responses = (res: Response, message: string, data?: object, error?: object) => {
-  error == null && logger.info(message)
-  error != null && logger.error(message)
-  return res.send({ message, data: data || {}, error: error || {} })
-}
